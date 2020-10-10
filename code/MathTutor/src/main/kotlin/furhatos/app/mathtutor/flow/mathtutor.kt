1d4578fcd7831732d4a0feae1058e2724cf1709e
yterhorst <ysbrandth@outlook.com>
2020-10-10 21:12:48 +0200

    Simple explanation loop

--- code/MathTutor/src/main/kotlin/furhatos/app/mathtutor/flow/mathtutor.kt
+++ code/MathTutor/src/main/kotlin/furhatos/app/mathtutor/flow/mathtutor.kt
@@ -1,6 +1,6 @@
 package furhatos.app.mathtutor.flow
 
-import furhatos.app.mathtutor.nlu.ShowInterest
+import furhatos.app.mathtutor.nlu.*
 import furhatos.flow.kotlin.*
 
 val Start = state(Interaction) {
@@ -14,11 +14,79 @@ val Start = state(Interaction) {
     onResponse<ShowInterest> {
         goto(SkillIntro)
     }
+
+    onResponse<Confused> {
+        goto(RobotIntro)
+    }
+}
+
+val RobotIntro = state(Interaction) {
+    onEntry {
+        furhat.ask("My name is Professor Euler and I can teach you everything you need to know about percentages. Would you like to learn more about percentages?")
+    }
+
+    onResponse<Yes> {
+        goto(SkillIntro)
+    }
+
+    onResponse<No> {
+        furhat.say("That's too bad. I'm afraid I can't help you then. Feel free to talk to me when you want to learn about it some other time")
+        goto(Idle)
+    }
 }
 
 val SkillIntro = state(Interaction) {
     onEntry {
         furhat.say("Great, Let's get started then!")
-        //goto(Explanation)
+        goto(Explanation1)
+    }
+}
+
+val Explanation1 : State = state(Interaction) {
+    onEntry {
+        furhat.ask("Percentage means 'per hundred'. You can express any value as broken up into 100 different parts, each being 1 percent. We use percentages to make calculations and comparisons between values easier. Are you with me so far?")
+    }
+
+    onResponse<Yes> {
+        //
+    }
+
+    onResponse<No> {
+        goto(Explanation2)
+    }
+
+    onResponse<DontUnderstand> {
+        goto(Explanation2)
+    }
+}
+
+val Explanation2 = state(Interaction) {
+    onEntry {
+        furhat.ask("A percentage is a ratio expressed as a fraction of 100. Do you understand this so far?")
+    }
+
+    onResponse<Yes> {
+        //
+    }
+
+    onResponse<No> {
+        goto(Encouragement)
+    }
+
+    onResponse<DontUnderstand> {
+        goto(Encouragement)
+    }
+}
+
+val Encouragement = state(Interaction) {
+    onEntry {
+        random(
+                furhat.say("Try to focus hard on the next part. I know you can do it!"),
+                furhat.say("Let's back up a little bit, I'm sure you will see the light."),
+                furhat.say("Alright, don't be frustrated if you don't get it the first time, let's try that again.")
+                // etc...
+        )
+        
+        goto(Explanation1)
     }
 }
--- code/MathTutor/src/main/kotlin/furhatos/app/mathtutor/nlu/nlu.kt
+++ code/MathTutor/src/main/kotlin/furhatos/app/mathtutor/nlu/nlu.kt
@@ -3,10 +3,49 @@ package furhatos.app.mathtutor.nlu
 import furhatos.nlu.*
 import furhatos.util.Language
 
-class ShowInterest: Intent() {
+class ShowInterest : Intent() {
     override fun getExamples(lang: Language): List<String> {
         return listOf(
-                "I would like to learn more about fractions",
-                "Can you teach me how to fraction?")
+                "I would like to learn more about percentages",
+                "Can you teach me how to calculate percentages?")
+    }
+}
+
+class Confused : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+                "Who are you?",
+                "What can you do?",
+                "What is this?")
+    }
+}
+
+class Yes : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+                "Yes",
+                "Sure",
+                "Yeah",
+                "Yes, please")
+    }
+}
+
+class No : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+                "No",
+                "No thanks",
+                "Nope",
+                "I'm good")
+    }
+}
+
+class DontUnderstand : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+                "Huh?",
+                "What?",
+                "I don't understand",
+                "I'm lost")
     }
 }
