fce127f964690358e6c1c931096b4d5ee045a25f
IPconfig <justin.haan92@gmail.com>
2020-10-27 03:09:45 +0100

    update flow of explanations

--- code/MathTutor/src/main/kotlin/furhatos/app/mathtutor/flow/explanation.kt
+++ code/MathTutor/src/main/kotlin/furhatos/app/mathtutor/flow/explanation.kt
@@ -47,11 +47,13 @@ val Explanation1: State = state(Interaction) {
 val Explanation2 = state(Interaction) {
     onEntry {
         furhat.ask("A percentage is nothing more than a ratio" +
-                "expressed as a fraction of 100. So we could also write 100% as 100/100, 50% as 50/100, 1% as 1/100 et cetera." +
-                " Do you understand this so far?")
+                "expressed as a fraction of 100. So we could also write 100% as 100/100, 50% as 50/100, 1% as 1/100, " +
+                "et cetera. Do you understand this so far?")
     }
 
-    onResponse<Yes> { goto(Explanation2Example) }
+    onResponse<Yes> {
+        furhat.say("Okay. Let me show you an example.")
+        goto(Explanation2Example) }
     onResponse<Repeat> {
         furhat.say("Okay, I will repeat my explanation again.")
         reentry() }
@@ -66,11 +68,14 @@ val Explanation2 = state(Interaction) {
 
 val Explanation1Example: State = state(Interaction) {
     onEntry {
-        furhat.say {"Let's try to compute 20% of 500. We start by calculating 1% of 500" + delay(250)}
-        furhat.say {"In our example we need to divide 500 by 100 to get 1%. " +
-                "This will give us 5." + delay(250)}
-        furhat.say("Remember that this is only 1%, and we would like to know 20% of 500"
-                + "So we multiply 5 by 20 to reach our final answer, which is 100")
+        furhat.say {
+            + "Let's try to compute 20% of 500."
+            + "We will start by calculating 1% of 500."
+            + "In our example we need to divide 500 by 100 to get 1%. "
+            + "This will give us 5. "
+            + delay(250)
+            + "Remember that this is only 1%, and we would like to know 20% of 500."
+            + "So we multiply 5 by 20 to reach our final answer, which is 100."}
         furhat.ask("Do you understand this?")
     }
     onResponse<Yes> { goto(ExerciseIntro) }
@@ -87,7 +92,7 @@ val Explanation1Example: State = state(Interaction) {
 
 val Explanation2Example : State = state(Interaction) {
     onEntry {
-        furhat.say("Let's try to compute 20% of 500. We start by writing 20% as 20/100 " +
+        furhat.say("Let's try to compute 20% of 500. We start by writing 20% as 20/100. " +
                 "We can then multiply our value by this fraction to reach our final answer. " +
                 "In our example, we can multiply 500 by 20/100. This will give 100, which is thus our final answer.")
         furhat.ask("Is this clear?")
